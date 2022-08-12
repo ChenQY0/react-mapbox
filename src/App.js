5901d08dd9329f6300f2f680d621317a2837ee67
cqy <chenqiuyi00@163.com>
2022-08-12 16:42:11 +0800

    Feat animate img

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import jsonData from "./times.json";
+
+// import dayjs, { mockDataTime } from './dayjs';
+// 引入子组件
+// import Children from './components/slider';
  
 mapboxgl.accessToken = '';
  
 export default function App() {
   const mapContainer = useRef(null);
   const map = useRef(null);
+  const datatime = useRef();
   const [lng, setLng] = useState(116.3883);
   const [lat, setLat] = useState(39.9289);
-  const [zoom, setZoom] = useState(9);
+  const [zoom, setZoom] = useState(3);
  
   useEffect(() => {
     if (map.current) return; // initialize map only once
@@ -19,7 +25,7 @@ export default function App() {
         "sources": {
             "raster-tiles": {
                 "type": "raster",
-                "tiles": ["http://webrd01.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}"],
+                "tiles": ["https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"],
                 "tileSize": 256
             }
         },
@@ -31,24 +37,51 @@ export default function App() {
             "maxzoom": 20
         }]
       },
+      // maxZoom: 5.99,
+      // minZoom: 4,
       center: [lng, lat],
       zoom: zoom,
     });
-    map.current.on('load', function() {
-      map.current.addLayer({
-        'id': 'wms-test-layer',
+    let currentOffset = 0;
+    let previousOffset = currentOffset;
+    function getTileServer(stepNumber, layers,opacity = 0) {
+      const url = ``;
+      map.current.addSource(layers,{
         'type': 'raster',
-        'source': {
-        'type': 'raster',
-        'tiles': [
-          'https://cdn-w.caiyunapp.com/p/mason/dev/20220803T033500Z/20220803T054000Z/radar/{z}/{y}/{x}/{z}_{y}_{x}.png'
-        ],
+        'tiles': [ url ],
         'tileSize': 256
-        },
-        'paint': {}
-        });
+      });
+      map.current.addLayer({
+        "id": layers,
+        "type": "raster",
+        "source": layers,
+        "paint": {
+          'raster-opacity': opacity,
+          "raster-fade-duration": 0
+        }
+      });
+      return map.current.getSource(layers)
+    }
+    map.current.on('load', function(){
+      const frames = [];
+      for (let i = 0; i < jsonData.length; i+= 1) {
+        const opacity = (i === 0 ) ? 1 : 0;
+        frames.push(getTileServer(i, 'radar-global'+i,opacity));
+      }
+      const waitTime = 500;
+      const stepTime = 400;
+      setTimeout(() => {
+        setInterval(() => {
+          previousOffset = currentOffset;
+          currentOffset += 1;
+          if (currentOffset === jsonData.length - 1) {
+            currentOffset = 0;
+          }
+          map.current.setPaintProperty(frames[previousOffset].id,'raster-opacity', 0)
+          map.current.setPaintProperty(frames[currentOffset].id,'raster-opacity', 1)
+        }, stepTime)
+      }, waitTime)
     })
-
   });
   
   useEffect(() => {
@@ -58,6 +91,10 @@ export default function App() {
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(parseInt(map.current.getZoom()));
     });
+    if(datatime.current){
+      clearInterval( datatime.current);
+    }
+
   });
  
   return (
@@ -66,6 +103,8 @@ export default function App() {
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
       <div ref={mapContainer} className="map-container" />
+      {/* <Children /> */}
     </div>
   );
+
 }
---
+++ src/components/slider.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react';
+
+
+import { Slider } from 'antd';
+
+export default class Children extends PureComponent {
+
+  render(){
+   
+    return(
+      <div className="sliderData">
+        <Slider />
+      </div>
+    )
+  }
+}
